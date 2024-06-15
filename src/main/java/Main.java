9cf880a7d393ecf2b1d9dcc406c4c3f846474bfc
JW-Bosma <jwbosma1969@gmail.com>
2024-06-15 12:26:44 +0200

    alle variabelen ingevoegd

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         String string_voorbeeld = "Dit is een voorbeeld";
-
-
-
+        String string_hello_world = "hello world";
+        boolean boolean_true = true;
+        int int_four = 4;
+        short short_minus_eight = -8;
+        float float_six_point_five = 6.5F;
+        double double_minus_two_point_three = -2.3;
 
 
         // Wijzig niets aan onderstaande System.out.println statements
@@ -20,7 +23,19 @@ public class Main {
 
         // Bonus: Wijs een nieuwe waarde toe aan een bestaande variabele
 
+        string_voorbeeld = "Dit is een aangepast voorbeeld";
+        boolean_true = false;
+        int_four = 8;
+        short_minus_eight = -4;
+        float_six_point_five = 3.45F;
+        double_minus_two_point_three = -9.13;
+
         System.out.println(string_voorbeeld); // String Dit is een aangepast voorbeeld
+        System.out.println(boolean_true); // boolean false
+        System.out.println(int_four); // int 8
+        System.out.println(short_minus_eight); // short -4
+        System.out.println(float_six_point_five); // float 3.45
+        System.out.println(double_minus_two_point_three); // double -9.13
     }
 
 }
